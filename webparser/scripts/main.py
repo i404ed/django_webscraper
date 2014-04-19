e7ee02ed7b9ff25dfedb4a3f5726d405fcad2dff
i404ed <harlancao@yahoo.com>
2014-04-18 20:36:33 -0500

    documentation

--- webparser/scripts/main.py
+++ webparser/scripts/main.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(__file__)
 def run():
     print "Running"
     html = os.path.join(BASE_DIR, 'cs473.html')
-    link = "https://courses.illinois.edu/cisapp/dispatcher/schedule/2013/fall/CS/421"
+    link = "https://courses.illinois.edu/cisapp/dispatcher/schedule/2012/fall/CS/232"
 
     # scrape = SoupParser.Parser(html, 0, "test")
     scrape = SoupParser.Parser(link, 1, "testURL")
